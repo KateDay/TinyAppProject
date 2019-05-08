98f4913ab40d250cdd56da43f222c954b0fea131
Kate <khummle@gmail.com>
2019-05-08 15:34:15 +0000

    redirecting the user to /urls/:shortURL upon submitting their long url as input

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 
 
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -43,19 +43,22 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urlsShow", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-   console.log(req.body);  // Log the POST request body to the console
-   res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
-
-
 function generateRandomString() {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 6; i++ ) {
+    let result           = '';
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 6; i++ ) {
        result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
+
+app.post("/urls", (req, res) => {
+   console.log(req.body);  // Log the POST request body to the console
+  let randString = generateRandomString();
+  urlDatabase[randString] = req.body.longURL
+
+  res.redirect("/urls/" + randString);         
+});
+
 
