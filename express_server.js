1a60257b5b2bb893d2510bec4d82b07b1be4c590
Kate <khummle@gmail.com>
2019-05-09 22:26:02 +0000

    fixing the registration so it does not take invalid entires and it does not accept exsisting emails

--- express_server.js
+++ express_server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser())
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -13,9 +13,10 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 const users = { 
     "userRandomID": {
-      id: "userRandomID", 
+      id: "userRandomID",
       email: "user@example.com", 
       password: "purple-monkey-dinosaur"
     },
@@ -24,8 +25,7 @@ const users = {
       email: "user2@example.com", 
       password: "dishwasher-funk"
     }
-}
-
+};
 
 app.get("/", (req, res) => {
   res.redirect("/urls");
@@ -44,7 +44,7 @@ app.get("/urls", (req, res) => {
   let templateVars = { 
     urls: urlDatabase,
     username: req.cookies["username"]
-};
+    };
   res.render("urls_index", templateVars);
 });
 
@@ -86,6 +86,25 @@ function generateRandomString() {
     return result;
 }
 
+function generateRandomID() {
+    let result           = '';
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < 10; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+
+function emailChecker (email) {
+    for (let id in users) {
+        if (email === users[id].email) {
+           return users[id]
+            }
+    }
+};
+
 app.post("/urls", (req, res) => {
    console.log(req.body);  // Log the POST request body to the console
   let randString = generateRandomString();
@@ -97,7 +116,7 @@ app.post("/urls/:shortURL/delete",(req, res) => {
     const shortURL = req.params.shortURL
 	delete urlDatabase[shortURL]
     res.redirect("/urls");
-})
+});
 
 app.post("/urls/:shortURL/update", (req, res) => {
 	console.log('I am editing a URL')
@@ -105,44 +124,45 @@ app.post("/urls/:shortURL/update", (req, res) => {
 	const longURL = req.body.longURL
 	urlDatabase[shortURL] = longURL
 	res.redirect("/urls");
-})
+});
 
 app.post("/login",(req, res) => {
     let login = req.body.username;
     let password = req.body.password;
 
-    res.cookie("password", password)
     res.cookie("username", login)
     console.log(`Username is: ${login}`);
     res.redirect("/urls");
-})
+});
 
 app.post("/logOut",(req, res) => {
     console.log(`${req.body.username} has logged out.`);
     res.clearCookie("username");
     res.redirect("/register");
-})
+});
 
 app.post("/register",(req, res) => {
     let email = req.body.email;
-    let password = bcrypt.HashSync(req.body.password);//this is where I HASH
+    let password =req.body.password;
+    let id = generateRandomID();
+    
+    let user = { id, email, password }; 
+    users[id] = user;
 
-    res.cookie("password", password)
-    res.cookie("email",email)
-    console.log(`Username is: ${email + password}`);
-    res.redirect("/urls");
-})
+    if(email == '' || password == '') {
+        res.status(400).render("/register")
+        } else if ( emailChecker(email) === false ){
+        res.status(400).render("/register")
+        } else {
+        res.cookie("username",email);
+        return {email,password}
+        console.log(`Username is: ${email} and Password is:${password}`);
+        res.redirect("/urls");
+        } 
+});
 
 
-
-
-// app.post("/logOut",(req, res) => {
-//     console.log(`${req.body.username} has logged out.`);
-//     res.clearCookie("username");
-//     res.redirect("/register");
-// })
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}!`);
-  });
-  
+});
