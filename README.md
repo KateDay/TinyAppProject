e433c5c776e2380f43c48ac3c783328124d28f2c
KateDay <khummle@gmail.com>
2019-05-17 13:32:25 -0600

    Making a README file with overview and instructions for user

---
+++ README.md
@@ -0,0 +1,41 @@
+# Welcome to TinyApp
+##The app that makes your urls smaller.
+
+TInyApp is a full stack web app that that shortens a long url, buil with express and Node.
+
+
+## Dependancies:
+- Node.js
+- Express
+- EJS
+- bcrypt
+- body-parser
+- cookie-session
+
+## Getting Started
+- Install all dependancies above by tpying "npm install" in your terminal.
+- start the server using Nodemon with "npm start"
+
+```
+npm install
+npm start
+```
+- once the server is up and running check it out on your browser. the port is 8080! 
+
+### How it works
+Register to get access. Once your'e in, the main page is going to display a list of the URLs you have. Since this is your first visit, You don't have any! Let's change that!
+Click the link "Create a new URL" link. 
+
+### Create a New URL
+Got a crazy long URL? Awesome! Type it in (or copy and paste it, I'm not gonna type out all that stuff...) and click the button. 
+
+Omit the HTTP part, I've got that covered for ya!... This will automatically bring you to a new page displaying the short link. Feel free to redirect yourself to your page just by clicking on the Short URL. It will open in a new tab, ready to go!
+
+OOoops, what's that?! I typed in the WRONG long address!! Stop panicing! You are able to edit your origional link, no worries!
+
+### Deleting unnessecary Links
+On our main page, you now have a bunch of links. Upon reexamining this list, you've decided that some are completely unnessecary. No problem! Just click the delete button and forget all about it. Please be careful though, clicking delete will get rid of it FOREVER. If you want it back, you will have to recreate it. 
+
+
+##This is Tiny App! 
+Thank you for getting Tiny with us!
--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const cookieSession = require('cookie-session');
 
-app.use(express.static("public"));
+app.use(express.static("/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(cookieSession({
@@ -15,15 +15,15 @@ app.use(cookieSession({
 }));
 
 
-const users = { 
+const users = {
     "userRandomID": {
       id: "userRandomID",
-      email: "user@example.com", 
+      email: "user@example.com",
       password: "purple-monkey-dinosaur"
     },
    "user2RandomID": {
-      id: "user2RandomID", 
-      email: "user2@example.com", 
+      id: "user2RandomID",
+      email: "user2@example.com",
       password: "dishwasher-funk"
     }
 };
@@ -62,7 +62,7 @@ function userLookup (email) {
 }
 
 function urlsForUser(loggedInId){
-    let dBforUser = {}; 
+    let dBforUser = {};
     for (let short in urlDatabase){
         if(loggedInId === urlDatabase[short].userID){
         dBforUser[short] = urlDatabase[short];
@@ -71,37 +71,36 @@ function urlsForUser(loggedInId){
     return dBforUser;
 }
 
-
+// All the GET Responses:
 app.get("/", (req, res) => {
-  res.redirect("/register");
+    res.redirect("/urls");
 });
 
-
 app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+    res.json(urlDatabase);
 });
 app.get("/u", (req, res) => {
     res.json(users);
-  });
+});
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/image", (req, res) => {
-    res.render(image);
-   });
+
+    res.render("Tiny.jpg");
+});
 
 app.get("/urls", (req, res) => {
     if (!req.session.userId) {
         res.redirect("/register");
     } else {
-        let templateVars = { 
-        urls: urlsForUser(req.session.userId),   
+        let templateVars = {
+        urls: urlsForUser(req.session.userId),
         user: users[req.session["userId"]],
         };
     res.render("urls_index", templateVars);
-    // console.log("these are my templateVariables: " + templateVars);
     }
 
     console.log("this is the reqSession: " + req.session.userId);
@@ -111,7 +110,7 @@ app.get("/urls/new", (req, res) => {
     if (!req.session.userId) {
         res.redirect("/register");
     } else {
-    let templateVars = { 
+    let templateVars = {
         shortURL: req.params.shortURL,
         longURL: urlDatabase[req.params.shortURL],
         user: users[req.session["userId"]],
@@ -125,7 +124,7 @@ app.get("/urls/:shortURL", (req, res) => {
     if (!req.session.userId) {
         res.redirect("/register");
     } else {
-    let templateVars = { 
+    let templateVars = {
         shortURL: req.params.shortURL,
         longURL: urlDatabase[req.params.shortURL].longURL,
         userId: req.session["userId"],
@@ -137,7 +136,7 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-    
+
     const longURL = urlDatabase[req.params.shortURL].longURL;
 console.log("the get for /:short" + longURL);
     res.redirect("http://" + longURL);
@@ -151,14 +150,15 @@ app.get("/register", (req, res) => {
 });
 
 
+// All the POST responses:
 app.post("/urls", (req, res) => {
-   console.log(req.body);  // Log the POST request body to the console
+    console.log(req.body); 
     let randString = generateRandomString();
     urlDatabase[randString] = {
         longURL: req.body.longURL,
         userID: req.session.userId
     };
-    res.redirect("/urls/" + randString);         
+    res.redirect("/urls/" + randString);
 });
 
 app.post("/urls/:shortURL/delete",(req, res) => {
@@ -167,12 +167,11 @@ app.post("/urls/:shortURL/delete",(req, res) => {
     if (urlDatabase[shortURL].userID === userID){
         delete urlDatabase[shortURL];
     }
-    
+
     res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/update", (req, res) => {
-	// console.log('I am editing a URL');
     const shortURL = req.params.shortURL;
 	const longURL = req.body.longURL;
 	urlDatabase[shortURL] = longURL;
@@ -184,17 +183,18 @@ app.post("/login",(req, res) => {
     let password = bcrypt.hashSync(req.body.password, saltRounds);
     let userDB = userLookup(email)
 
-    if(userDB) { 
-// check password.... if that's true login otherwise say try again'
-    if (bcrypt.compareSync(req.body.password, userDB.password)) {
-        req.session.userId = userDB.id;   
-        console.log(`user ${email} is logged in`);
-        res.redirect("/urls");
-    } else {
-        // window.alert("Oops, try again")
-        res.render("registration")
-    }    
-    } else {res.status(400 + "Oops try again").render("registration")
+    if(userDB) {
+        if (bcrypt.compareSync(req.body.password, userDB.password)) {
+            req.session.userId = userDB.id;
+            console.log(`user ${email} is logged in`);
+            res.redirect("/urls");
+        } else {
+            alert("Ooops try again")
+            res.render("registration")
+        }
+        } else {
+            alert("Oops, try again")
+            res.status(400).render("registration")
     }
 });
 
@@ -208,22 +208,21 @@ app.post("/register",(req, res) => {
     let id = generateRandomID();
     let userDB = userLookup(email)
     let password = bcrypt.hashSync(req.body.password, saltRounds)
-    
-    let user = { id, email, password }; 
+
+    let user = { id, email, password };
     users[id] = user;
-    
+
 
     if(email == '' || password == '') {
         res.status(400).render("registration")
         } else if ( email !== userDB ){
-            req.session.userId = id;        
-            // return {email,password}
-        console.log(`user is: ${email} and Password is:${password}`);
-        res.redirect("/urls");
-        } 
+            req.session.userId = id;
+            res.redirect("/urls");
+        }
         console.log(user);
 });
 
+// Port Listening
 app.listen(PORT, () => {
     console.log(`TinyApp listening on port ${PORT}!`);
 });
